3765ba5b686dc616db032cf80927695f1487dccd
ZhReimu <xiaokangning@yijiupi.com>
2023-06-21 09:00:48 +0800

    初始化仓库

---
+++ src/main/java/com/mrx/githubcodespacetest/GithubCodeSpaceTestApplication.java
@@ -0,0 +1,13 @@
+package com.mrx.githubcodespacetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubCodeSpaceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubCodeSpaceTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrx/githubcodespacetest/GithubCodeSpaceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrx.githubcodespacetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCodeSpaceTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
